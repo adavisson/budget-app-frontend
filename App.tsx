d6b17f1b26e93434a1af8db145312a8883b14fd8
Andrew Davisson <akdavisson4@gmail.com>
2022-01-07 11:17:58 -0700

    inputs for login screen

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name='Home'
           component={HomeScreen}
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,21 +1,78 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import {
+  Keyboard,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 
 export const LoginScreen = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const loginHandler = () => {};
+
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-      <StatusBar style='auto' />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.loginView}>
+        <Text style={styles.header}>BUDJ</Text>
+        <TextInput
+          value={email}
+          mode='flat'
+          label='email'
+          style={styles.input}
+          underlineColor='#7F39FB'
+          onChangeText={text => setEmail(text)}
+          autoComplete={false}
+        />
+        <TextInput
+          value={password}
+          mode='flat'
+          label='Password'
+          style={styles.input}
+          underlineColor='#7F39FB'
+          secureTextEntry={true}
+          onChangeText={text => setPassword(text)}
+          autoComplete={false}
+        />
+        <Button
+          mode='contained'
+          style={styles.button}
+          labelStyle={styles.buttonText}
+          onPress={loginHandler}
+        >
+          LOG IN
+        </Button>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  loginView: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#03DAC5',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    marginTop: '45%',
+    fontSize: 48,
+    letterSpacing: 15,
+    color: '#7F39FB',
+  },
+  input: {
+    width: '75%',
+    backgroundColor: 'transparent',
+    color: '#7F39FB',
+  },
+  button: {
+    marginTop: 40,
+    width: 150,
+    margin: 10,
+  },
+  buttonText: {
+    color: '#03DAC5',
   },
 });
