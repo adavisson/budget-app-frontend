ee4fe45b2dca29051387698f442783b275bc6a71
Andrew Davisson <akdavisson4@gmail.com>
2022-01-15 15:21:56 -0700

    add inputs

--- src/screens/AddCategoryScreen.tsx
+++ src/screens/AddCategoryScreen.tsx
@@ -1,8 +1,8 @@
 import { RouteProp } from '@react-navigation/core';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button } from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 
 interface IAddCategoryProps {
   navigation: NativeStackNavigationProp<any>;
@@ -14,21 +14,75 @@ export const AddCategoryScreen: React.FC<IAddCategoryProps> = ({
   route,
 }) => {
   const { userId } = route.params;
+  const [name, setName] = useState<string>('');
+  const [color, setColor] = useState<string>('');
+  const [step, setStep] = useState<number>(0);
+
+  const steps = [
+    {
+      value: name,
+      label: 'category name',
+      setter: setName,
+    },
+    {
+      value: color,
+      label: 'category color',
+      setter: setColor,
+    },
+  ];
 
   const goBack = () => {
     navigation.navigate('Home', { userId });
   };
 
+  const handleButtonPress = () => {
+    if (step < 1) {
+      setStep(step + 1);
+    } else if (step === 1) {
+      //submit
+    }
+  };
+
+  const handleBackButtonPress = () => {
+    if (step > 0) {
+      setStep(step - 1);
+    }
+  };
+
+  const getButtonText = () => (step < 1 ? 'NEXT' : 'SUBMIT');
+
   return (
     <View style={styles.container}>
       <Button
-        style={styles.backButton}
-        labelStyle={styles.backButtonText}
+        style={styles.goBackButton}
+        labelStyle={styles.goBackButtonText}
         onPress={goBack}
       >
-        {`< HOME`}
+        {`<- HOME`}
       </Button>
-      <Text style={styles.headerText}>ADD CATEGORY</Text>
+      <TextInput
+        value={steps[step].value}
+        mode='flat'
+        label={steps[step].label}
+        style={styles.input}
+        underlineColor='#7F39FB'
+        onChangeText={value => steps[step].setter(value)}
+        autoComplete={false}
+        autoCapitalize='none'
+      />
+      <Button
+        mode='contained'
+        style={styles.button}
+        labelStyle={styles.buttonText}
+        onPress={handleButtonPress}
+      >
+        {getButtonText()}
+      </Button>
+      {step > 0 ? (
+        <TouchableOpacity onPress={handleBackButtonPress}>
+          <Text style={styles.backButton}>{'<- Back'}</Text>
+        </TouchableOpacity>
+      ) : null}
     </View>
   );
 };
@@ -45,12 +99,30 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: '#7F39FB',
   },
-  backButton: {
+  goBackButton: {
     position: 'absolute',
     top: 75,
     left: 20,
   },
-  backButtonText: {
+  goBackButtonText: {
+    color: '#7F39FB',
+  },
+  input: {
+    marginTop: '55%',
+    width: '75%',
+    backgroundColor: 'transparent',
+    color: '#7F39FB',
+  },
+  button: {
+    marginTop: 40,
+    marginBottom: 20,
+    width: 150,
+    margin: 10,
+  },
+  buttonText: {
+    color: '#03DAC5',
+  },
+  backButton: {
     color: '#7F39FB',
   },
 });
